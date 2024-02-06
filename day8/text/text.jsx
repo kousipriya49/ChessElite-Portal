dbc5f44c3f76ca160ed72a6b66b7afdc0b93835e
PRIYA <727721euit116@skcet.ac.in>
2024-02-06 11:20:11 +0530

    Add files via upload

---
+++ day8/text/text.jsx
@@ -0,0 +1,25 @@
+// GalleryHeader.jsx
+import React from "react";
+import "../../src/styles/text1.css";
+const GalleryHeader = () => {
+  return (
+    <>
+    
+    <div className="bodytournaments">
+    <center>
+      <div className="tournament-heading">
+      <div className="text-effect">
+        <h1 className="neon" data-text="Tounaments time" contentEditable>
+          
+        </h1>
+        <div className="gradient"></div>
+        <div className="spotlight"></div>
+      </div>
+      </div>
+    </center>
+    </div>
+    </>
+  );
+};
+
+export default GalleryHeader;
